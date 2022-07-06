6f24ecaa88472195be0671cc97473fb7d274942f
zerry101 <macone394@gmail.com>
2022-07-06 20:23:14 +0530

    m

--- cloudkitchenweb/src/app/app-routing.module.ts
+++ cloudkitchenweb/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { MenuComponent } from './Pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'about',component:AboutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cloudkitchenweb/src/app/app.module.ts
+++ cloudkitchenweb/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Sharepage/navbar/navbar.component';
+import { FooterComponent } from './Sharepage/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { MenuComponent } from './Pages/menu/menu.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { ContactComponent } from './Pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
