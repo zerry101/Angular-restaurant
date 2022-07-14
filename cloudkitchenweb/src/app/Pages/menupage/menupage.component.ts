6ed7021c398ff263255786da8dd642d09f5a35ac
zerry101 <macone394@gmail.com>
2022-07-14 17:34:43 +0530

    m

--- cloudkitchenweb/src/app/Pages/menupage/menupage.component.ts
+++ cloudkitchenweb/src/app/Pages/menupage/menupage.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderDetailsService } from 'src/app/service/order-details.service';
 
 @Component({
   selector: 'app-menupage',
   templateUrl: './menupage.component.html',
-  styleUrls: ['./menupage.component.scss']
+  styleUrls: ['./menupage.component.scss'],
 })
 export class MenupageComponent implements OnInit {
-
-  constructor() { }
+  getMenuId: any;
+  menuData:any;
+  constructor(
+    private param: ActivatedRoute,
+    private service: OrderDetailsService
+  ) {}
 
   ngOnInit(): void {
+    this.getMenuId = this.param.snapshot.paramMap.get('id');
+    console.log(this.getMenuId, 'getMenuId');
+    if(this.getMenuId)
+    {
+      this.menuData= this.service.foodDetails.filter((value)=>{
+        return value.id==this.getMenuId;
+      })
+    }
   }
-
 }
